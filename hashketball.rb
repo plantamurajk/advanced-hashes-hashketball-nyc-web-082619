ac1637cf5b027b0be74a0eb9256614808ee0d0e8
John Plantamura <plantamurajk@gmail.com>
2019-08-18 16:56:19 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,19 @@
 # Write your code here!
 
+require "pry"
 
+def game_hash
+  hash = {:home => {}, :away => {}}
+  
+  hash[:home] = {:team_name => "Brooklyn Nets", :colors => [], :players => []}
+  
+  puts hash
+end
 
+def create_player(name, number, shoe, points, rebounds, assists, steals, blocks, dunks)
+  
+  player = {:player_name => name, :number => number, :shoe => shoe, :points => points, :rebounds => rebounds, :assists => assists, :steals => steals, :blocks => blocks, :slam_dunks => dunks}
+  
+end
 
-
-
-
-
-
-
+puts create_player('Alan Anderson', 0, 16, 22, 12, 12, 3, 1, 1)
